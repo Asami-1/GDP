a50be1fe42496471797f1a03eb640e7d13030b7a
Quentin Fever <quentinfever@rn-dynamic-187-184.wless.cranfield.ac.uk>
2023-03-14 17:15:36 +0000

    Data split done

--- pose/prep.py
+++ pose/prep.py
@@ -1,110 +1,140 @@
 
-print("aaaaa")
-import os
-#path = "Output_vitposea"
+
+
 
 import os
-
-def rename_dirs(path):
-    # go through Agressive et Peace
-    for subdir in ['Agressive', 'Peace']:
-        subdir_path = os.path.join(path, subdir)
-        if os.path.isdir(subdir_path):
-            # go through les sous-dossiers
-            for subsubdir_name in os.listdir(subdir_path):
-                subsubdir_path = os.path.join(subdir_path, subsubdir_name)
-                if os.path.isdir(subsubdir_path):
-                    # Renommer le sous-dossier en utilisant le nouveau nom
-                    new_subsubdir_name = f"{subdir}_{subsubdir_name}"
-                    new_subsubdir_path = os.path.join(subdir_path, new_subsubdir_name)
-                    os.rename(subsubdir_path, new_subsubdir_path)
+"""
+def rename_subfolders(path, prefix):
+    for i, foldername in enumerate(sorted(os.listdir(path), key=lambda x: int(x) if not x.startswith(".") else float("inf"))):
+        oldname = os.path.join(path, foldername)
+        newname = os.path.join(path, f"{prefix}{i+1}")
+        os.rename(oldname, newname)
+        print(f"Renamed {oldname} to {newname}")
 
 
-rename_dirs("TEST") 
 
+path = r"result_conflict_cam2"
+prefix = "Agressive_cam2_"
+rename_subfolders(path, prefix)
+"""
+
+
+
+
+"""
+import os
+import shutil
+
+def merge_folders(source_folder, destination_folder):
+
+    
+    # On récupère les noms des sous-dossiers de source_folder
+    subfolders = [f.path for f in os.scandir(source_folder) if f.is_dir()]
+    
+    # Pour chaque sous-dossier, on vérifie s'il existe déjà dans destination_folder
+    for subfolder in subfolders:
+        subfolder_name = os.path.basename(subfolder)
+        destination_subfolder = os.path.join(destination_folder, subfolder_name)
         
+        # Si le sous-dossier n'existe pas encore dans destination_folder, on le copie simplement
+        if not os.path.exists(destination_subfolder):
+            shutil.copytree(subfolder, destination_subfolder)
+        
+        # Si le sous-dossier existe déjà dans destination_folder, on le renomme en ajoutant un numéro
+        else:
+            i = 2
+            while os.path.exists(destination_subfolder):
+                destination_subfolder = os.path.join(destination_folder, subfolder_name + f" ({i})")
+                i += 1
+            shutil.copytree(subfolder, destination_subfolder)
 
-#doesnt work  because folders (and not files) inside Agressive and Peace
-    # def split_data(input_path, output_path, split_ratio=0.75):
-    #     # Creatre subpath
-    #     train_path = os.path.join(output_path, "Train")
-    #     test_path = os.path.join(output_path, "Test")
-    #     os.makedirs(train_path, exist_ok=True)
-    #     os.makedirs(test_path, exist_ok=True)
-    # Go through Agressive and Peace 
-    # for subdir in ['Agressive', 'Peace']:
-    #     subdir_path = os.path.join(input_path, subdir)
-    #     if os.path.isdir(subdir_path):
-    #         # Sélectionner les fichiers dans le sous-dossier
-    #         files = [file_name for file_name in os.listdir(subdir_path) if os.path.isfile(os.path.join(subdir_path, file_name))]
-    #         # Shuffle the files, not really necessary 
-    #         random.shuffle(files)
-    #         # Separate 
-    #         num_files = len(files)
-    #         num_train = int(num_files * split_ratio)
-    #         train_files = files[:num_train]
-    #         test_files = files[num_train:]
-    #         # Copier les fichiers dans les dossiers Train et Test
-    #         for file_name in train_files:
-    #             src_path = os.path.join(subdir_path, file_name)
-    #             dst_path = os.path.join(train_path, subdir, file_name)
-    #             os.makedirs(os.path.dirname(dst_path), exist_ok=True)
-    #             shutil.copy(src_path, dst_path)
-    #         for file_name in test_files:
-    #             src_path = os.path.join(subdir_path, file_name)
-    #             dst_path = os.path.join(test_path, subdir, file_name)
-    #             os.makedirs(os.path.dirname(dst_path), exist_ok=True)
-    #             shutil.copy(src_path, dst_path)
-    #         print(f"{num_files} fichiers dans le sous-dossier {subdir}: {num_train} dans Train, {num_files - num_train} dans Test.")
-
-    # # Compter le nombre total de fichiers dans les sous-dossiers Train
-    # num_train_aggressive = len(os.listdir(os.path.join(train_path, "Agressive")))
-    # num_train_peace = len(os.listdir(os.path.join(train_path, "Peace")))
-    # total_train_files = num_train_aggressive + num_train_peace
-
-# import os
-# import shutil
-# import random
-
-# def split_data(input_path, output_path, split_ratio=0.75):
-#     # Créer le dossier Data avec les sous-dossiers Train et Test
-#     train_path = os.path.join(output_path, "Train")
-#     test_path = os.path.join(output_path, "Test")
-#     os.makedirs(train_path, exist_ok=True)
-#     os.makedirs(test_path, exist_ok=True)
-
-#     # Parcourir les sous-dossiers Agressive et Peace
-#     for subdir in ['Agressive', 'Peace']:
-#         subdir_path = os.path.join(input_path, subdir)
-#         if os.path.isdir(subdir_path):
-#             # Parcourir  tous les fichiers dans le sous-dossier
-#             files = []
-#             for dirpath, dirnames, filenames in os.walk(subdir_path):
-#                 for filename in filenames:
-#                     files.append(os.path.join(dirpath, filename))
-#             # Mélanger les fichiers aléatoirement
-#             random.shuffle(files)
-#             # Séparer les fichiers en train et test
-#             num_files = len(files)
-#             num_train = int(num_files * split_ratio)
-#             train_files = files[:num_train]
-#             test_files = files[num_train:]
-#             # Copier les fichiers dans les dossiers Train et Test
-#             for file_path in train_files:
-#                 src_path = file_path
-#                 dst_path = os.path.join(train_path, subdir, os.path.relpath(file_path, subdir_path))
-#                 os.makedirs(os.path.dirname(dst_path), exist_ok=True)
-#                 shutil.copy(src_path, dst_path)
-#             for file_path in test_files:
-#                 src_path = file_path
-#                 dst_path = os.path.join(test_path, subdir, os.path.relpath(file_path, subdir_path))
-#                 os.makedirs(os.path.dirname(dst_path), exist_ok=True)
-#                 shutil.copy(src_path, dst_path)
-
-#     # Compter le nombre total de fichiers dans les sous-dossiers Train
-#     num_train_aggressive = len([name for name in os.listdir(os.path.join(train_path, "Agressive")) if os.path.isfile(os.path.join(train_path, "Agressive", name))])
-#     num_train_peace = len([name for name in os.listdir(os.path.join(train_path, "Peace")) if os.path.isfile(os.path.join(train_path, "Peace", name))])
-#     total_train_files = num_train_aggressive + num_train_peace
+# Exemple d'utilisation de la fonction merge_folders
+source_folder = "Peace_test"
+destination_folder = "Agressive_test"
+merge_folders(source_folder, destination_folder)
+"""
 
 
-#split_data("TEST"," TEST_OUTPUT")    
+
+"""
+import os
+
+def rename_subfolders(folder_path, prefix):
+
+    
+    # On récupère les noms des sous-dossiers du dossier
+    subfolders = [f.path for f in os.scandir(folder_path) if f.is_dir()]
+    
+    # On renomme chaque sous-dossier avec le nouveau nom
+    for i, subfolder in enumerate(subfolders):
+        new_name = f"{prefix}_{i+1}"
+        os.rename(subfolder, os.path.join(folder_path, new_name))
+
+# Exemple d'utilisation de la fonction rename_subfolders
+folder_path = "result_peace_cam1"
+prefix = "Peace"
+rename_subfolders(folder_path, prefix)
+"""
+ 
+
+
+
+import os
+import random
+import shutil
+
+def split_folders(folder_path, train_ratio=0.75):
+    
+    #Split subfolders in a folder into train and test subfolders.
+    
+    #Parameters:
+        #folder_path (str): The path to the folder to be split.
+        #train_ratio (float): The ratio of subfolders to be placed in the train folder.
+        
+    #Returns:
+    #    None
+    
+    # Create the train and test folders
+    train_folder = os.path.join(folder_path, 'train')
+    test_folder = os.path.join(folder_path, 'test')
+    os.makedirs(train_folder, exist_ok=True)
+    os.makedirs(test_folder, exist_ok=True)
+    
+    # Get the subfolder names
+    subfolder_names = os.listdir(folder_path)
+    
+    # Shuffle the subfolder names
+    random.shuffle(subfolder_names)
+    
+    # Split the subfolder names into train and test sets
+    num_train = int(train_ratio * len(subfolder_names))
+    train_subfolder_names = subfolder_names[:num_train]
+    test_subfolder_names = subfolder_names[num_train:]
+    
+    # Move the train subfolders
+    for subfolder_name in train_subfolder_names:
+        subfolder_path = os.path.join(folder_path, subfolder_name)
+        train_subfolder_path = os.path.join(train_folder, subfolder_name)
+        os.makedirs(train_subfolder_path, exist_ok=True)
+        
+        # Iterate through the files in the subfolder
+        for file_name in os.listdir(subfolder_path):
+            file_path = os.path.join(subfolder_path, file_name)
+            if os.path.isfile(file_path) and file_name.endswith('.json'):
+                train_file_path = os.path.join(train_subfolder_path, file_name)
+                shutil.copy(file_path, train_file_path)
+    
+    # Move the test subfolders
+    for subfolder_name in test_subfolder_names:
+        subfolder_path = os.path.join(folder_path, subfolder_name)
+        test_subfolder_path = os.path.join(test_folder, subfolder_name)
+        os.makedirs(test_subfolder_path, exist_ok=True)
+        
+        # Iterate through the files in the subfolder
+        for file_name in os.listdir(subfolder_path):
+            file_path = os.path.join(subfolder_path, file_name)
+            if os.path.isfile(file_path) and file_name.endswith('.json'):
+                test_file_path = os.path.join(test_subfolder_path, file_name)
+                shutil.copy(file_path, test_file_path)
+
+split_folders("Agressive copy", train_ratio=0.75)
